91c52f1611908e5e6f28728a11cb5f43f0fc3426
DESKTOP-BPHRV40\nuwan <nuwandharshana4@gmail.com>
2019-04-21 12:57:07 +0530

    add state variables and dynamic components

--- src/App.js
+++ src/App.js
@@ -1,13 +1,40 @@
 import React, { Component } from 'react';
 import './App.css';
+import Person from './Person/Person';
 
 class App extends Component {
+  state = {
+    persons:[
+      {name:"Dhanushka", age:38},
+      {name:"Hasantha", age:26},
+      {name:"Pramith", age:29},
+    ],
+    otherState:'another state value'
+  }
+
+  switchNameHandler = () => {
+    // this.state.persons[0].name = // dont use , react wont identify state changes and rerender dom
+    this.setState({
+      persons:[
+        {name:"Dhanushka Rumesh", age:40},
+        {name:"Hasantha Prabath", age:27},
+        {name:"Pramith Chathuranga", age:30},
+      ]
+    })
+  }
+
   render() {
     return (
+      //use one root element
       <div className="App">
         <h1>I am a react app</h1>
+        <button onClick={this.switchNameHandler}>Switch Users</button>
+        <Person name={this.state.persons[0].name} age={this.state.persons[0].age}/>
+        <Person name={this.state.persons[1].name} age={this.state.persons[1].age}>My Hobbies: Racing</Person>
+        <Person name={this.state.persons[2].name} age={this.state.persons[2].age}/>
       </div>
     );
+    // return React.createElement('div',{className:'App'},React.createElement('h1',null,'I am a react app')); // actual react code example
   }
 }
 
