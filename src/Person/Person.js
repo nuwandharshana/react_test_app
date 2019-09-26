4a317b56b8edcb1ab1636d1b3ff2398aa412deba
nuwan-mdosl <nuwan.d@mydigitaloffice.lk>
2019-09-26 15:50:07 +0530

    fix grammar issue

--- src/Person/Person.js
+++ src/Person/Person.js
@@ -5,7 +5,7 @@ import './Person.css'
 const person = (props) =>{
     return (
         <div className="Person">
-            <p onClick={props.click}>I am a {props.name}. I am {props.age} years old.</p>
+            <p onClick={props.click}>I am {props.name}. I am {props.age} years old.</p>
             <p>{props.children}</p>
             <input type="text" onChange={props.changed} value={props.name}/>
         </div>
