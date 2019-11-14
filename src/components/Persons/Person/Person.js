11f6284d19b3bd81b8618b463ed311767dd62e34
nuwan-mdosl <nuwan.d@mydigitaloffice.lk>
2019-11-14 15:25:47 +0530

    add prop validations

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react'
+import PropTypes from 'prop-types'
 import classes from './Person.css'
 import Aux from '../../../hoc/Aux';
 import withClass from '../../../hoc/withClass';
@@ -32,6 +33,13 @@ class Person extends Component {
         //     <input key="3" type="text" onChange={this.props.changed} value={this.props.name} />
         // ]
     }
+}
+
+Person.propTypes = {
+    click:PropTypes.func,
+    name:PropTypes.string,
+    age:PropTypes.number,
+    changed:PropTypes.func
 }
 
 export default withClass(Person,classes.Person);
