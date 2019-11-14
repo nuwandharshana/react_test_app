4cbc5e97947f1cb16df1fdb5a0b278492792268f
nuwan-mdosl <nuwan.d@mydigitaloffice.lk>
2019-11-14 13:18:34 +0530

    hoc as a stateful component

--- src/hoc/withClass.js
+++ src/hoc/withClass.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React, {Component} from 'react'
+
+// const withClass = (WrappedComponent,ClassName)=>{
+//     return (props)=>(
+//         <div className={ClassName}>
+//             <WrappedComponent {...props}/>
+//         </div>
+//     )
+// }
 
 const withClass = (WrappedComponent,ClassName)=>{
-    return (props)=>(
-        <div className={ClassName}>
-            <WrappedComponent {...props}/>
-        </div>
-    )
+    return class extends Component{
+        render(){
+            return (
+                <div className={ClassName}>
+                    <WrappedComponent {...this.props} />
+                </div>
+            )
+        }
+    }
 }
 
 export default withClass
